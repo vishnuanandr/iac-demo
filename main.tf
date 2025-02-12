98f7611cae2f6a06fb9d46db034b4818d47ea7a3
Vishnu Anand R <42373744+vishnuanandr@users.noreply.github.com>
2025-02-12 15:38:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-#  access_key = ""
-#  secret_key = ""
-#  token = ""
+  access_key = "AKIATTSKFQ53IVCVDH4U"
+  secret_key = "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"
 }
 
 # Create VPC
